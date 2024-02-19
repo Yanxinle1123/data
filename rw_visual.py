3565f571c4acf034d8468e866650c07c8ee2eb63
yanxinle <1020121123@qq.com>
2024-02-19 21:00:11 +0800

    更新

--- rw_visual.py
+++ rw_visual.py
@@ -2,13 +2,27 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-# 创建一个RandomWalk实例
-rw = RandomWalk()
-rw.fill_walk()
+while True:
+    # 创建一个RandomWalk实例
+    rw = RandomWalk(50_000)
+    rw.fill_walk()
 
-# 将所有的点都绘制出来
-plt.style.use('classic')
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
-ax.set_aspect('equal')
-plt.savefig('squares_plot2.png', bbox_inches='tight')
+    # 将所有的点都绘制出来
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=1)
+    ax.set_aspect('equal')
+
+    # 突出起点和终点
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+
+    # 隐藏坐标轴
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    plt.show()
+    keep_running = input("还要随机游走吗？(y/n): ")
+    if keep_running == "n":
+        break
