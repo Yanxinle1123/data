582be4129521fe508df5c4b5f8b23c83b99f0388
yanxinle <1020121123@qq.com>
2024-02-19 18:48:31 +0800

    添加忽略文件

--- scatter_squares.py
+++ scatter_squares.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
 ax.scatter(2, 4, s=200)
-a
+
 # 设置图题并给坐标轴加上标签
 ax.set_title("Square Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
