950d4acd7deb09c202e1f3d5ab75129dd14951bd
yanxinle <1020121123@qq.com>
2024-02-27 13:03:36 +0800

    更新

--- die_visual.py
+++ die_visual.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 from die import Die
 
 # 创建一个D6
@@ -5,8 +7,19 @@ die = Die()
 
 # 掷几次骰子并将结果存储在一个列表里
 results = []
-for roll_num in range(100):
+for roll_num in range(1000):
     result = die.roll()
     results.append(result)
 
-print(results)
+# 分析结果
+frequencies = []
+poss_results = range(1, die.num_sides + 1)
+for value in poss_results:
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+print(frequencies)
+
+# 对结果进行可视化
+plt.bar(poss_results, frequencies, edgecolor='black')
+plt.show()
