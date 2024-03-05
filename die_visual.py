f288f44159b8bbb204a5c492947063864f22bb75
yanxinle <1020121123@qq.com>
2024-03-05 13:13:47 +0800

    更新die_visual

--- die_visual.py
+++ die_visual.py
@@ -1,45 +1,27 @@
-import matplotlib.pyplot as plt
+from LeleDrawDesigns.dice_analysis import dice_analysis
+from LeleDrawDesigns.histogram import Histogram
 from LeleDrawDesigns.line_chart import LineChart
 
-from die import Die
-
-# 创建一个D6
-die = Die()
-
-# 掷几次骰子并将结果存储在一个列表里
-results = []
-for roll_num in range(1000):
-    result = die.roll()
-    results.append(result)
-
-# 分析结果
-frequencies = []
-poss_results = range(1, die.num_sides + 1)
-for value in poss_results:
-    frequency = results.count(value)
-    frequencies.append(frequency)
-
-print(frequencies)
-
-# 对结果进行可视化
-title = 'Results of Rolling One D6 1,000 Times'
-xlabel = 'Result'
-ylabel = 'Frequency of Result'
-
-plt.bar(poss_results, frequencies, color='blue', edgecolor='black')
-plt.title(title, fontsize=20)
-plt.xlabel(xlabel, fontsize=15)
-plt.ylabel(ylabel, fontsize=15)
-plt.show()
+poss_results, frequencies = dice_analysis()
 
 input_values = []
 for i in range(len(frequencies)):
     input_values.append(i + 1)
 
-straight_line_chart = LineChart(input_values=input_values, squares=frequencies, style='straight',
-                                skin='Solarize_Light2', set_title=title, set_title_fontsize=20, set_xlabel=xlabel,
-                                set_xlabel_fontsize=15, set_ylabel=ylabel, set_ylabel_fontsize=15)
+title = 'Results of Rolling One D6 1,000 Times'
+xlabel = 'Result'
+ylabel = 'Frequency of Result'
 
-curved_line_chart = LineChart(input_values=input_values, squares=frequencies, style='curved', skin='Solarize_Light2',
-                              set_title=title, set_title_fontsize=20, set_xlabel=xlabel, set_xlabel_fontsize=15,
-                              set_ylabel=ylabel, set_ylabel_fontsize=15)
+histogram = Histogram(poss_results, frequencies, skin='Solarize_Light2', title=title, title_fontsize=20,
+                      x_label=xlabel, x_label_fontsize=20, y_label=ylabel, y_label_fontsize=20, color='green',
+                      edgecolor='black')
+
+straight_line_chart = LineChart(input_values=input_values, squares=frequencies, style='straight',
+                                line_color='green', skin='Solarize_Light2', set_title=title,
+                                set_title_fontsize=20, set_x_label=xlabel, set_x_label_fontsize=20,
+                                set_y_label=ylabel, set_y_label_fontsize=20, line_width=3)
+
+curved_line_chart = LineChart(input_values=input_values, squares=frequencies, style='curved',
+                              line_color='green', skin='Solarize_Light2', set_title=title,
+                              set_title_fontsize=20, set_x_label=xlabel, set_x_label_fontsize=20,
+                              set_y_label=ylabel, set_y_label_fontsize=20, line_width=3)
