7ef8f2c8d3a4f115e996b1e22a64d04610b3682e
yanxinle <1020121123@qq.com>
2024-03-08 13:27:12 +0800

    更新die_visual

--- die_visual.py
+++ die_visual.py
@@ -1,6 +1,7 @@
 from LeleDrawDesigns.dice_analysis import dice_analysis
 from LeleDrawDesigns.histogram import Histogram
 from LeleDrawDesigns.line_chart import LineChart
+from LeleDrawDesigns.obtain_all_skin import obtain_all_skin
 
 poss_results, frequencies = dice_analysis()
 
@@ -25,3 +26,5 @@ LineChart(input_values=input_values, squares=frequencies, style='curved',
           line_color='green', skin='Solarize_Light2', set_title=title,
           set_title_fontsize=20, set_x_label=xlabel, set_x_label_fontsize=20,
           set_y_label=ylabel, set_y_label_fontsize=20, line_width=3)
+
+obtain_all_skin()
