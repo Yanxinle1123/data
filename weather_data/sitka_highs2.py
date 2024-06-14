3aede770dfc1571833bff6720d3b0ef7045f0a8f
yanxinle <1020121123@qq.com>
2024-06-14 13:07:27 +0800

    更新

--- weather_data/sitka_highs2.py
+++ weather_data/sitka_highs2.py
@@ -2,7 +2,10 @@ import csv
 from datetime import datetime
 from pathlib import Path
 
+import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy.interpolate import interp1d
 
 path = Path('weather_data_csv/sitka_weather_07-2021_simple.csv')
 lines = path.read_text().splitlines()
@@ -21,7 +24,13 @@ for row in reader:
 # 根据最高温度绘图
 plt.style.use('Solarize_Light2')
 fig, ax = plt.subplots(figsize=(15, 10))
-ax.plot(dates, highs, color='red')
+
+# 插值并绘制曲线
+date_nums = mdates.date2num(dates)
+f = interp1d(date_nums, highs, kind='cubic')
+date_nums_interp = np.linspace(date_nums.min(), date_nums.max(), len(dates) * 10)
+highs_interp = f(date_nums_interp)
+ax.plot(mdates.num2date(date_nums_interp), highs_interp, color='red')
 
 # 设置绘制的格式
 ax.set_title("Daily High Temperatures, July 2021", fontsize=24)
@@ -30,4 +39,4 @@ fig.autofmt_xdate()
 ax.set_ylabel("Temperature(F)", fontsize=16)
 ax.tick_params(labelsize=16)
 
-plt.savefig('sitka_highs')
+plt.savefig('sitka_highs2')
